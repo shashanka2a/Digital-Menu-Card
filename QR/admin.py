e9bfdadec6c96b086eed12837e9435556f7860a9
varma7 <pradeepvarma107@gmail.com>
2021-02-11 16:50:22 +0530

    add: cart
    Signed-off-by: varma7 <pradeepvarma107@gmail.com>

--- QR/admin.py
+++ QR/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Item,User
+from .models import Item,User,Cart,CartItem
 # Register your models here.
 
 
 admin.site.register(User)
 admin.site.register(Item)
+admin.site.register(Cart)
+admin.site.register(CartItem)
--- QR/urls.py
+++ QR/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import Contact, store
+from .views import Contact, store,addCartV,cartV,remove_from_cart
 from django.conf import settings
 from django.conf.urls.static import static
+
+
+app_name = "qr"
+
 urlpatterns = [
-    path('', store, name='store'),
-    path('<str:itemname>/', store, name='store'),
-    path('contact/', Contact, name='contact')
+    path('store/<str:itemname>/', store, name='store'),
+    path('additem/<str:itemid>/',addCartV,name='additemtocart'),
+    path('contact/', Contact, name='contact'),
+    path('cart/',cartV,name='cart'),
+    path('remove/<int:id>',remove_from_cart,name='removefromcart')
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
