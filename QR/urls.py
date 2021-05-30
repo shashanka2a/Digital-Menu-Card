8bbceaa44bd314c6c6f3e8f9ea9d9357056b35ef
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2021-05-30 19:22:03 +0530

    Update urls.py

--- QR/urls.py
+++ QR/urls.py
@@ -5,14 +5,16 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('store/<int:pk>/', store, name='store'),
-    path('contact/', Contact, name='contact'),
     path('cart/add/<int:id>/', cart_add, name='cart_add'),
     path('cart/item_clear/<int:id>/', item_clear, name='item_clear'),
     path('cart/item_increment/<int:id>/',item_increment, name='item_increment'),
     path('cart/item_decrement/<int:id>/',item_decrement, name='item_decrement'),
     path('cart/cart_clear/',cart_clear, name='cart_clear'),
     path('cart/cart-detail/',cart_detail,name='cart_detail'),
-    path('orderAlP/',order,name='orderit')
+    path('orderAlP/',order,name='orderit'),
+    path('orderstatus/<int:pk>',order_status,name='order_status'),
+    path('order_update/<int:id>/',order_update,name='order_update'),
+    path('ordersdisplay/',orders_display,name='orders_display')
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
